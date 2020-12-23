b735ca13682081db862117dfc79e9df2f73a312b
sannidhikasturi <67547611+sannidhikasturi@users.noreply.github.com>
2020-12-24 01:15:40 +0530

    Update main.js

--- js1/main.js
+++ js1/main.js
@@ -34,7 +34,7 @@
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-    function validate (input) {
+    function validateform(input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
