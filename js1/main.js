6a3da2a8a1ecde8c9d243ca98dd9e4144411efe9
sannidhikasturi <67547611+sannidhikasturi@users.noreply.github.com>
2020-12-24 12:33:04 +0530

    Update main.js

--- js1/main.js
+++ js1/main.js
@@ -30,18 +30,11 @@
            hideValidate(this);
         });
     });
-    function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
 
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
+                return true;
             }
         }
         else {
